55aa11e4e4d141b826df0917f9c1322174adc8bd
BriarEmber <conaway.devyn669@gmail.com>
2026-02-02 01:45:29 +0000

    finalized 2/1

--- FutureTime.py
+++ FutureTime.py
@@ -1,7 +1,7 @@
 #FutureTime.py
-#Name:
-#Date:
-#Assignment:
+#Name:Devyn Conaway
+#Date:2/01/26
+#Assignment:Lab 2
 
 # datetime will allow us to access the system date and time.
 import datetime
@@ -9,15 +9,24 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  
+  currentHour =  now.hour
   currentMinute = now.minute
-
-  print (currentHour, currentMinute) #this is just for checking, we should delete it later
-
+  
+  #print (currentHour, currentMinute) #this is just for checking, we should delete it later
+    
   #TODO:
   #Ask user for hours
   #Ask user for minutes
+  moreHours = int(input("How many hours in the future would you like to see?"))
+  moreMins = int(input("How many minutes in the future would you like to see?"))
 
+  futureMins = (currentMinute + moreMins) % 60 
+  extraHour = (currentMinute + moreMins) // 60
+  futureHours = (currentHour + moreHours + extraHour) % 24
+  extraHours = (currentHour + moreHours + extraHour) // 24
+  
+  print(f'The future time will be {futureHours:02}:{futureMins:02}.')
   #Calculate the time after the user-supplied time has passed.
 
   #Do not use any if statements in calculating the time.
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,18 +1,33 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name:Devyn Conaway
+#Date:2/01/2026
+#Assignment:Lab 2
+#Purpose:Variable and Arithmetic
 
 #We will need random for this program, import to use this package.
 import random
 
-def main():
+def main(random):
   #Create a list of your responses.
+  
   print("Magic 8 Ball")
+  
   #Prompt the user for their question.
-
+  answers = ("Yes", "No", "Try again later", "could be", "probably at one time or another", 
+             "Try not doing that", "Have you considered another way?", "Not likely", 
+             "The possibilities are endless", "Go for a walk", "Only you can answer that", 
+             "You need chocolate", "Use GPS", " Don't do that", "More thought is required", 
+             "That's not your color", "Be prepared", "Knowledge is valuable", 
+             "Don't underestimate yourself", "Concentrate on the important things")
+  
+  input("Ask your question to determine your fate.")
   #Answer question randomly with one of the options from your earlier list.
-
+  length = len(answers)
+  r = random.random() * length
+  r = int(r)
+  
+  response = answers[r]
+  print(response)
 
 if __name__ == '__main__':
-  main()
+  main(random)
